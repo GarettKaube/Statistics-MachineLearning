08c5f7b90dbb914c689c526f065dd01174a521fb
Garett Kaube <108091263+GarettKaube@users.noreply.github.com>
2024-04-01 18:16:06 -0600

    Update timeseriescrossvalidation.py
    added some documentations

--- machinelearning/tools/timeseriesCV/timeseriescrossvalidation.py
+++ machinelearning/tools/timeseriesCV/timeseriescrossvalidation.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pandas as pd
-from prophet.diagnostics import cross_validation
-from prophet.diagnostics import performance_metrics
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_absolute_percentage_error
@@ -9,19 +7,19 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn import feature_selection
 
-
+# Expected model schema for TimeSeriesCrossValidator
 class Model:
     def __init__(self):
         self.model = None
     
-    def train(self, train_df):
-        # Fit the model
+    def train(self, train_df) -> pd.Series:
+        # Fit the model and return a series of predictions
         return None
     
     def predict(self, train_df, test_df, *args):
         """ Return the test predictions and insample predictions.
         Returns:
-            - predictions: pd.Series of the forecasts.
+            - predictions: pd.Series of the forecasts for future.
             - train_predictions: pd.Series of the insample forecasts.
         """
         predictions, train_predictions = [None, None]
@@ -30,7 +28,7 @@ class Model:
     
             
 class TimeSeriesCrossValidator:
-    def __init__(self, model, fold_size, cv_step=1, **kwargs):
+    def __init__(self, model, target_name, fold_size, cv_step=1, **kwargs):
         """
         Input:
             - model_type: str either "ARIMA", or "Prophet" is supported
@@ -40,6 +38,8 @@ class TimeSeriesCrossValidator:
         self.model = model
         self.size = fold_size
         self.cv_step = cv_step
+
+        self.target_name = target_name
         
         # Store models that are trained and tested
         self.model_list = []
@@ -87,8 +87,10 @@ class TimeSeriesCrossValidator:
         self.pca_subset = subset
     
     
-    def feat_selection(self, n_features):
-        """ Enables feature selection to be done during cross-validation.
+    def enable_feature_selection(self, n_features):
+        """ 
+        Enables in-fold feature selection to avoid bias.
+
         Input:
             -n_features: Number of features to select
         Returns:
@@ -99,51 +101,82 @@ class TimeSeriesCrossValidator:
     
             
     def select_features(self, train, test):
-        # Feature selection based on pearson correlation
-        pearson = feature_selection.SelectKBest(feature_selection.mutual_info_regression, k=self.n_features)
-        pearson.fit_transform(train.drop(['y','ds'], axis=1), train['y'])
-        cols = pearson.get_feature_names_out()
+        """
+        Apply feature selection
+        Returns:
+            - train: pd.DataFrame containing selected columns, target, and ds column
+            - test
+        """
+        selection = feature_selection.SelectKBest(
+            feature_selection.mutual_info_regression, 
+            k=self.n_features
+        )
+        selection.fit_transform(train.drop([self.target_name,'ds'], axis=1), train['y'])
+        cols = selection.get_feature_names_out()
         print(f"feature selection chose {cols}")
         
-        train = train[list(cols)+ ['y','ds']]
-        test = test[list(cols)+ ['y','ds']]
+        train = train[list(cols)+ [self.target_name, 'ds']]
+        test = test[list(cols)+ [self.target_name, 'ds']]
         return train, test
     
     
     def dimension_reduction(self, train, test=None):
+        """
+        Applys PCA to the features in-fold to avoid bias.
+        """
         pca_cols = [f"pc{i}" for i in range(1, self.pca_n_components + 1)]
-        pca_ = PCA(n_components=self.pca_n_components)
+        pca = PCA(n_components=self.pca_n_components)
+
         if not self.pca_subset:
             cols = train.columns.drop(['y', 'ds'])
         else:
             cols = self.pca_subset
 
-        train_dim_red = pd.DataFrame(pca_.fit_transform(train.loc[:,cols]), columns=pca_cols)
-        train = pd.concat([train[train.columns.drop(cols)].reset_index(drop=True), train_dim_red], axis=1)
+        # Apply PCA to train set
+        train_dim_red = pd.DataFrame(
+            pca.fit_transform(train.loc[:,cols]), 
+            columns=pca_cols
+        )
+        
+        train = pd.concat(
+            [train[train.columns.drop(cols)].reset_index(drop=True), 
+             train_dim_red], 
+             axis=1
+        )
 
+        # Apply PCA to test set
         if test is not None:
-            test_dim_red  = pd.DataFrame(pca_.transform(test[cols]), columns=pca_cols)
-            test = pd.concat([test[test.columns.drop(cols)].reset_index(drop=True), test_dim_red], axis=1)
+            test_dim_red  = pd.DataFrame(pca.transform(test[cols]), columns=pca_cols)
+            test = pd.concat(
+                [test[test.columns.drop(cols)].reset_index(drop=True), test_dim_red], 
+                axis=1
+            )
             return train, test
         else:
             return train
         
     
     def scaling(self, train, test):
-        """ Standardize variables
+        """ 
+        Make train and test sets have centered mean and unit variance
+        Returns:
+            train, test pd.DataFrames
         """
         scaler_ = StandardScaler(copy=False)
-        train.loc[:,train.columns.drop(['y','ds'])]= scaler_.fit_transform(train.drop(['y','ds'], axis=1))
-        test.loc[:, test.columns.drop(['y','ds'])]= scaler_.transform(test.drop(['y','ds'], axis=1))
+        train.loc[:,train.columns.drop([self.target_name,'ds'])]= scaler_.fit_transform(train.drop(['y','ds'], axis=1))
+        test.loc[:, test.columns.drop([self.target_name,'ds'])]= scaler_.transform(test.drop(['y','ds'], axis=1))
         return train, test
     
     
     def calculate_performance_metrics(self, actual_train, actual_test, test_predictions, train_predictions, dates):
+        """
+        Calculates MSE, RMSE, MAE and MAPE given test predictions and actual values in the test set.
+        """
         predict_naive = [actual_train.iloc[-1]] # Naive forecast. will only be used if fold_size = 1
         y_hat = test_predictions
         y_hat_train = train_predictions
         
-        self.mse[str(dates[0])] = mean_squared_error(y_hat, actual_test).item()
+        self.mse[str(dates.iloc[0])] = mean_squared_error(y_hat, actual_test).item()
         self.train_mse.append(mean_squared_error(y_hat_train, actual_train))
         self.mae.append(mean_absolute_error(y_hat, actual_test).item())
         self.mape.append(mean_absolute_percentage_error(y_hat, actual_test).item())
@@ -159,23 +192,23 @@ class TimeSeriesCrossValidator:
             - start: int index indicating the initial size of the train set
             - end: int index indicating what index to end cross-validation
         Returns:
-            - 
+            - None
         """
         import math
+        # Calculate the number of folds
         n_folds = math.floor(((end-start) - self.size + self.cv_step) / self.cv_step)
         assert n_folds > 0, "Start index must be larger than end index"
         continue_ = True
         
         print("N_folds:", n_folds)
-        print(f"Features: {data.columns.drop(['y', 'ds'])}")
+        print(f"Features: {data.columns.drop([self.target_name, 'ds'])}")
         
         while continue_:
             for fold in range(n_folds+1):
-                
-                next_index = start + fold*self.cv_step # increase size of train
+                # increase next_index
+                next_index = start + fold*self.cv_step
+
                 data_copy = data.copy()
-                # new train set
-                train = data_copy[:next_index]
                     
                 # Reset model each fold
                 self.model.__init__(**self.model_args)
@@ -183,12 +216,21 @@ class TimeSeriesCrossValidator:
                 if next_index + self.size <= end and fold <= n_folds: # make sure index doesn't go beyond the length of end
                     train, test = self.__cross_validation_step(train, data_copy, next_index)
                     self.__performance(train, test, fold, print_test_dates)
-
                 else:
                     continue_ = False      
     
     
-    def __cross_validation_step(self, train, df, next_index):
+    def __cross_validation_step(self, df:pd.DataFrame, next_index):
+        """
+        Inputs:
+            - df: pd.DataFrame with data to split
+            - next_index: index to split df on
+        Returns:
+            - train: updated train set
+            - test: updated test set
+        """
+        # New train and test sets
+        train = df[:next_index]
         test = df[next_index:next_index + self.size] # new test set
        
         if self.feature_selection:
@@ -200,7 +242,7 @@ class TimeSeriesCrossValidator:
         if self.pca:
             train, test = self.dimension_reduction(train, test)
         # Train and predict
-        self.model.train(train)
+        self.model.train(train, self.target_name)
         print(test.shape)
         self.model_list.append(self.model.model)
         return train, test
@@ -217,8 +259,8 @@ class TimeSeriesCrossValidator:
         predictions, train_predictions, = self.model.predict(train, test, self.size)
 
         # Check performance
-        actual_test = test['y']
-        actual_train = train['y']
+        actual_test = test[self.target_name]
+        actual_train = train[self.target_name]
         self.calculate_performance_metrics(actual_train, actual_test, predictions, train_predictions, test['ds'])
 
         if self.predict_naive and print_test_dates:
@@ -229,7 +271,8 @@ class TimeSeriesCrossValidator:
         
     
     def print_metrics(self):
-        """Prints mean squared error (MSE), root MSE, pooled RMSE, mean absolute error, and mean absolute percentage error for test and train sets.
+        """
+        Prints mean squared error (MSE), root MSE, pooled RMSE, mean absolute error, and mean absolute percentage error for test and train sets.
         """
         mse = list(self.mse.values())
         print("MSE:", np.mean(mse))
